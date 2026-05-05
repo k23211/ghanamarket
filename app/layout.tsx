47c321e082485b622cbb72ffd87b7063ab18d5c4
Albert <k2311363@gmail.com>
2026-05-05 15:35:30 -0700

    initial setup

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "GhanaMarket",
+  description: "Ghana e-commerce store",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
